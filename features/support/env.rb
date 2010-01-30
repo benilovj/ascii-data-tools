d1b50f3f5cf5ab1df0f108ada53edd7955a72698
Jake Benilov <benilov@gmail.com>
2010-01-30 16:42:38 +0100

    fixed cucumber config so that storytests run under ruby1.9

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,3 +1,5 @@
-require 'ascii-data-tools'
+lib_path = File.expand_path("#{File.dirname(__FILE__)}/../../lib")
+$LOAD_PATH.unshift lib_path unless $LOAD_PATH.include?(lib_path)
 
+require 'ascii-data-tools'
 require 'ascii-data-tools/configuration_printer'
