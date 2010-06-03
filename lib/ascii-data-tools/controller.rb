b082cf636a5fae2b9f0bd6e3507ac00d1212ef30
benilovj <benilov@gmail.com>
2010-06-03 21:28:19 +0200

    refactored the controller code, simplifying the pipeline concept added first tests and code for the normaliser

--- lib/ascii-data-tools/controller.rb
+++ lib/ascii-data-tools/controller.rb
@@ -5,19 +5,28 @@ module AsciiDataTools
         @configuration = configuration
         @configuration.error_info_with_usage unless @configuration.valid?
       end
-
-      def run
-        @type_determiner = RecordType::TypeDeterminer.new(@configuration.record_types)
-        @formatter       = Formatting::Formatter.new
-
-        @record_source   = Record::Source.new(@configuration.input_source, @type_determiner)
-        @record_sink     = Record::Sink.new(@configuration.output_stream, @formatter)
-
-        Record::Pipeline.new(@record_source, @record_sink).start_flow
-      end
       
       def self.from_command_line(command_line_arguments)
         new(Configuration.new(command_line_arguments))
+      end
+      
+      def run
+        formatter = Formatting::Formatter.new
+        type_determiner = RecordType::TypeDeterminer.new(@configuration.record_types)
+        
+        pipeline = Record::TransformingPipeline.new do |encoded_record, filename|
+          type = type_determiner.determine_type_for(encoded_record, filename)
+          decoded_record = type.decode(encoded_record)
+          formatter.format(decoded_record)
+        end
+        pipeline.stream(@configuration.input_source, @configuration.output_stream)
+      end
+    end
+    
+    class NormalisationController < CatController
+      def run
+        pipeline = Record::TransformingPipeline.new {|encoded_record, filename| encoded_record}
+        pipeline.stream(@configuration.input_source, @configuration.output_stream)
       end
     end
   end
--- lib/ascii-data-tools/record.rb
+++ lib/ascii-data-tools/record.rb
@@ -30,48 +30,19 @@ module AsciiDataTools
       end
     end
     
-    class Source
-      include Enumerable
-      
-      def initialize(input_source, type_determiner)
-        @input_source = input_source
-        @type_determiner = type_determiner
+    class TransformingPipeline
+      def initialize(&block)
+        @transformer_block = block
       end
       
-      def each
-        until @input_source.stream.eof?
-          encoded_record = @input_source.stream.readline
-          type = @type_determiner.determine_type_for(encoded_record, @input_source.filename)
-          yield type.decode(encoded_record)
+      def stream(record_source, output_stream)
+        until record_source.stream.eof?
+          original_record = record_source.stream.readline
+          transformed_record = @transformer_block[original_record, record_source.filename]
+          output_stream << transformed_record
         end
-      end
-    end
-    
-    class Sink
-      def initialize(output_stream, transformer)
-        @output_stream = output_stream
-        @transformer = transformer
-      end
-      
-      def receive(record)
-        @output_stream << @transformer.transform(record)
-      end
-      
-      def flush_and_close
-        @output_stream.flush
-        @output_stream.close
-      end
-    end
-  
-    class Pipeline
-      def initialize(record_source, record_sink)
-        @record_source = record_source
-        @record_sink = record_sink
-      end
-      
-      def start_flow
-        @record_source.each {|record| @record_sink.receive(record)}
-        @record_sink.flush_and_close
+        output_stream.flush
+        output_stream.close
       end
     end
   end
